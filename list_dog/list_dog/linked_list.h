850e36b185666dd4e57bc88e3df97f0ff375389a
Jazmin Gamboa Chacon <jazmin.gamboa.chacon@est.una.ac.cr>
2024-01-27 13:00:51 -0600

    Add LinkedList Class

---
+++ list_dog/list_dog/linked_list.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "node.h"
+
+class LinkedList
+{
+private:
+    Node* head;
+
+public:
+    LinkedList();
+    ~LinkedList();
+    void insertAtTheBeginning(Dog* newDog);
+    void insertAtTheEnd(Dog* newDog);
+    void insertSortedByAge(Dog* newDog);
+    void deleteAtStartup();
+    void deleteAtTheEnd();
+    void deleteAnIndex(Dog* newDog);
+    void deleteByPosition(Node* actualPosition, Node* previusPosition);
+    bool searchForADogByName(Dog* newDog);
+    void findAllDogsUnderACertainAge(int maxAge);
+    void sortTheListByAgeAndDisplayIt();
+    void print();
+};
