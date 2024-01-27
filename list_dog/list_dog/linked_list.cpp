72a4091b361b8710cf19be2946bf48123cf3a69b
Jazmin Gamboa Chacon <jazmin.gamboa.chacon@est.una.ac.cr>
2024-01-27 13:05:05 -0600

    Add insertAtTheBeginning(Dog* newDog)

--- list_dog/list_dog/linked_list.cpp
+++ list_dog/list_dog/linked_list.cpp
@@ -1,1 +1,37 @@
 #include "linked_list.h"
+
+LinkedList::LinkedList()
+{
+	head = nullptr;
+}
+
+LinkedList::~LinkedList()
+{
+	Node* current = head;
+	if (head == nullptr)
+	{
+		return;
+	}
+
+	while (current != nullptr)
+	{
+		head = head->getNext();
+		delete[] current;
+		current = head;
+	}
+}
+
+void LinkedList::insertAtTheBeginning(Dog* newDog)
+{
+	Node* newNode = new Node(newDog);
+
+	if (head != nullptr)
+	{
+		newNode->setNext(head);
+		head = newNode;
+	}
+	else
+	{
+		head = newNode;
+	}
+}
